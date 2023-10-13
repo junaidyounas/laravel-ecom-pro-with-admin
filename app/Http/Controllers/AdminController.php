ab452838d18371b87944d039e7bf7fccfb77c627
Junaid Younas <junaidammar2013@gmail.com>
2023-10-13 12:41:57 +0500

    add product

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Category; // Import the Category model
+use App\Models\Product; // Import the Category model
 
 use Illuminate\Http\Request;
 
@@ -25,5 +26,32 @@ class AdminController extends Controller
         $data=Category::find($id);
         $data->delete();
         return redirect()->back()->with('message', 'Category deleted successfully');
+    }
+
+    public function view_product(){
+        $category = Category::all();
+        return view('admin.product', compact('category'));
+    }
+
+    public function add_product(Request $request){
+        $product = new Product();
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->category = $request->category;
+        $product->price = $request->price;
+        $product->discount_price = $request->discount_price;
+        $product->quantity = $request->quantity;
+
+        $image = $request->image;
+        $imagename=time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('product', $imagename);
+        $product->image=$imagename;
+
+        $product->save();
+
+        return redirect()->back()->with('message', 'Product added successfully');
+
+
     }
 }
---
+++ resources/views/admin/product.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('admin.css')
+</head>
+
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/_sidebar.html -->
+        @include('admin.sidebar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/_navbar.html -->
+            @include('admin.header')
+            <!-- partial -->
+            <div class="main-panel">
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                        {{ session()->get('message') }}
+                    </div>
+                @endif
+                <div class="content-wrapper">
+                    {{-- Add Product Module --}}
+                    <div class="col-md-12 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Add Product</h4>
+                                <p class="card-description"> Fill the form to add product </p>
+                                <form action="{{ url('/add_product') }}" method="POST" enctype="multipart/form-data"
+                                    class="forms-sample" method="POST" action="{{ url('add_category') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="exampleInputUsername1">Title*</label>
+                                        <input type="text" name="title" class="form-control"
+                                            id="exampleInputUsername1" placeholder="category">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputUsername1">Description</label>
+                                        <input type="text" name="description" class="form-control"
+                                            id="exampleInputUsername1" placeholder="Description">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputUsername1">Price*</label>
+                                        <input type="number" name="price" class="form-control"
+                                            id="exampleInputUsername1" placeholder="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputUsername1">Quantity</label>
+                                        <input type="number" name="Quantity" class="form-control"
+                                            id="exampleInputUsername1" placeholder="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputUsername1">Discount Price</label>
+                                        <input type="text" name="dicount_price" class="form-control"
+                                            id="exampleInputUsername1" placeholder="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleSelectGender">Gender</label>
+                                        <select name="category" class="form-control" id="exampleSelectGender">
+                                            @foreach ($category as $category)
+                                                <option value="{{ $category->category_name }}">{{ $category->category_name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>File upload</label>
+                                        <input type="file" name="image" required class="file-upload-default">
+                                        <div class="input-group col-xs-12">
+                                            <input type="text" class="form-control file-upload-info" disabled
+                                                placeholder="Upload Image">
+                                            <span class="input-group-append">
+                                                <button class="file-upload-browse btn btn-primary"
+                                                    type="button">Upload</button>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary mr-2">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- Add Product module ends --}}
+
+                </div>
+
+
+            </div>
+            <!-- main-panel ends -->
+
+        </div>
+
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    @include('admin.script')
+    <!-- End custom js for this page -->
+    <script>
+        function delete_confirmation(ev) {
+            ev.preventDefault();
+            var urlToRedirect = ev.currentTarget.getAttribute('href');
+            swal({
+                title: 'Delete Confirmation',
+                text: 'Are you sure you want to delete',
+                icon: 'warning',
+                buttons: true,
+                dangerMode: true,
+            }).then((willCancel) => {
+                if (willCancel) {
+                    window.location.href = urlToRedirect;
+                }
+            })
+        }
+    </script>
+</body>
+
+</html>
